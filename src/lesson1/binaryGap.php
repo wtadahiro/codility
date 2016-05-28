c2876927e4b5fc8daf54a65f13700413511121ce
tadahiro.wada <tadahiro.wada@d8aspring.com>
2016-05-28 17:04:15 +0900

    make it testable

--- src/lesson1/binaryGap.php
+++ src/lesson1/binaryGap.php
@@ -1,24 +1,28 @@
 <?php
+namespace Wtadahiro\Codility\Lesson1;
 
-function solution($n){
-    $binary = decbin($n);
-    $ary = str_split($binary);
+class BinaryGap
+{
 
-    $tmp = [];
-    $maxLength = 0;
-    foreach($ary as $flag){
-        if($flag == '0'){
-            array_push($tmp, '0');
-        }elseif($flag == '1'){
-            if($maxLength < count($tmp)){
-                $maxLength = count($tmp);
+    public function solution($n) 
+    {
+        $binary = decbin($n);
+        $ary = str_split($binary);
+
+        $tmp = [];
+        $maxLength = 0;
+        foreach($ary as $flag){
+            if($flag == '0'){
+                $tmp[] = '0';
+            }elseif($flag == '1'){
+                if($maxLength < count($tmp)){
+                    $maxLength = count($tmp);
+                }
+                $tmp = [];
             }
-            $tmp = [];
         }
+
+        return $maxLength;
     }
 
-    return $maxLength;
 }
-
-echo solution($argv[1]);
-echo PHP_EOL;
